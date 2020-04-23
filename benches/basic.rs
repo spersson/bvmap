8b7e10abfccbc58f199a4737a9a32e4d49540c4b
Simon Persson <simon.persson@mykolab.com>
2020-04-23 22:38:30 +0800

    Add hopslotmap

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 use rand::{thread_rng, Rng};
-use slotmap::{DefaultKey, SlotMap};
+use slotmap::{DefaultKey, SlotMap, HopSlotMap};
 use stash::{Stash, UniqueStash};
 use store::Store;
 
@@ -10,6 +10,7 @@ fn inserts(c: &mut Criterion) {
     let s2: Stash<usize, usize> = Stash::new();
     let s3: UniqueStash<usize> = UniqueStash::new();
     let s4: SlotMap<DefaultKey, usize> = SlotMap::new();
+    let s5: HopSlotMap<DefaultKey, usize> = HopSlotMap::new();
 
     let mut g = c.benchmark_group("Inserts");
     g.bench_function("Store", |b| {
@@ -56,6 +57,17 @@ fn inserts(c: &mut Criterion) {
             BatchSize::SmallInput,
         );
     });
+    g.bench_function("HopSlotMap", |b| {
+        b.iter_batched_ref(
+            || s5.clone(),
+            |i| {
+                for a in 0..size {
+                    i.insert(a);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
 }
 
 fn reinserts(c: &mut Criterion) {
@@ -66,17 +78,22 @@ fn reinserts(c: &mut Criterion) {
     let mut s3k = Vec::new();
     let mut s4: SlotMap<DefaultKey, usize> = SlotMap::new();
     let mut s4k = Vec::new();
+    let mut s5: HopSlotMap<DefaultKey, usize> = HopSlotMap::new();
+    let mut s5k = Vec::new();
+
     for a in 0..size {
         s1.insert(a);
         s2.put(a);
         s3k.push(s3.put(a));
         s4k.push(s4.insert(a));
+        s5k.push(s5.insert(a));
     }
     for a in 0..size {
         s1.remove(a);
         s2.take(a);
         s3.take(s3k[a]);
         s4.remove(s4k[a]);
+        s5.remove(s5k[a]);
     }
     let mut g = c.benchmark_group("Re-inserts");
     g.bench_function("Store", |b| {
@@ -123,6 +140,17 @@ fn reinserts(c: &mut Criterion) {
             BatchSize::SmallInput,
         );
     });
+    g.bench_function("HopSlotMap", |b| {
+        b.iter_batched_ref(
+            || s5.clone(),
+            |i| {
+                for a in 0..size {
+                    i.insert(a);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
 }
 fn remove(c: &mut Criterion) {
     let size = 10_000;
@@ -133,11 +161,15 @@ fn remove(c: &mut Criterion) {
     let mut s3k = Vec::new();
     let mut s4: SlotMap<DefaultKey, usize> = SlotMap::new();
     let mut s4k = Vec::new();
+    let mut s5: HopSlotMap<DefaultKey, usize> = HopSlotMap::new();
+    let mut s5k = Vec::new();
+
     for a in 0..size {
         s1.insert(a);
         s2.put(a);
         s3k.push(s3.put(a));
         s4k.push(s4.insert(a));
+        s5k.push(s5.insert(a));
     }
 
     let mut g = c.benchmark_group("Remove");
@@ -185,6 +217,17 @@ fn remove(c: &mut Criterion) {
             BatchSize::SmallInput,
         );
     });
+    g.bench_function("HopSlotMap", |b| {
+        b.iter_batched_ref(
+            || s5.clone(),
+            |i| {
+                for _ in 0..size {
+                    i.remove(s5k[rng.gen_range(0, size)]);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
 }
 
 fn get(c: &mut Criterion) {
@@ -196,11 +239,15 @@ fn get(c: &mut Criterion) {
     let mut s3k = Vec::new();
     let mut s4: SlotMap<DefaultKey, usize> = SlotMap::new();
     let mut s4k = Vec::new();
+    let mut s5: HopSlotMap<DefaultKey, usize> = HopSlotMap::new();
+    let mut s5k = Vec::new();
+
     for a in 0..size {
         s1.insert(a);
         s2.put(a);
         s3k.push(s3.put(a));
         s4k.push(s4.insert(a));
+        s5k.push(s5.insert(a));
     }
     let mut g = c.benchmark_group("Get");
     g.bench_function("Store", |b| {
@@ -247,22 +294,39 @@ fn get(c: &mut Criterion) {
             BatchSize::SmallInput,
         );
     });
+    g.bench_function("HopSlotMap", |b| {
+        b.iter_batched_ref(
+            || s5.clone(),
+            |i| {
+                for _ in 0..size {
+                    black_box(i.get(s5k[rng.gen_range(0, size)]));
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
 }
 
 fn iter(c: &mut Criterion) {
     let size = 10_000;
     let mut rng = thread_rng();
     let mut s1: Store<usize, usize> = Store::new();
+    let mut s1k = Vec::new();
     let mut s2: Stash<usize, usize> = Stash::new();
+    let mut s2k = Vec::new();
     let mut s3: UniqueStash<usize> = UniqueStash::new();
     let mut s3k = Vec::new();
     let mut s4: SlotMap<DefaultKey, usize> = SlotMap::new();
     let mut s4k = Vec::new();
+    let mut s5: HopSlotMap<DefaultKey, usize> = HopSlotMap::new();
+    let mut s5k = Vec::new();
+
     for a in 0..size {
-        s1.insert(a);
-        s2.put(a);
+        s1k.push(s1.insert(a));
+        s2k.push(s2.put(a));
         s3k.push(s3.put(a));
         s4k.push(s4.insert(a));
+        s5k.push(s5.insert(a));
     }
 
     let mut g = c.benchmark_group("Iterate");
@@ -310,14 +374,31 @@ fn iter(c: &mut Criterion) {
             BatchSize::SmallInput,
         )
     });
+    g.bench_function("HopSlotMap", |b| {
+        b.iter_batched_ref(
+            || s5.clone(),
+            |i| {
+                for a in i.iter() {
+                    black_box(a);
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
     g.finish();
 
-    for _ in 0..size / 2 {
-        let k = rng.gen_range(0, size);
-        s1.remove(k);
-        s2.take(k);
+    for subset in ((size / 2)..size).rev() {
+        let k = rng.gen_range(0, subset);
+        s1.remove(s1k[k]);
+        s1k.swap_remove(k);
+        s2.take(s2k[k]);
+        s2k.swap_remove(k);
         s3.take(s3k[k]);
+        s3k.swap_remove(k);
         s4.remove(s4k[k]);
+        s4k.swap_remove(k);
+        s5.remove(s5k[k]);
+        s5k.swap_remove(k);
     }
 
     let mut g = c.benchmark_group("Iterate half-full");
@@ -357,6 +438,17 @@ fn iter(c: &mut Criterion) {
     g.bench_function("SlotMap", |b| {
         b.iter_batched_ref(
             || s4.clone(),
+            |i| {
+                for a in i.iter() {
+                    black_box(a);
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+    g.bench_function("HopSlotMap", |b| {
+        b.iter_batched_ref(
+            || s5.clone(),
             |i| {
                 for a in i.iter() {
                     black_box(a);
