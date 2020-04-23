387051b65d87b199b3dafce723deec979a1acd7a
Simon Persson <simon.persson@mykolab.com>
2020-04-23 20:34:08 +0800

    Add re-inserts benchmark

--- benches/basic.rs
+++ benches/basic.rs
@@ -4,14 +4,14 @@ use slotmap::{DefaultKey, SlotMap};
 use stash::{Stash, UniqueStash};
 use store::Store;
 
-fn fresh_inserts(c: &mut Criterion) {
+fn inserts(c: &mut Criterion) {
     let size = 10_000;
     let s1: Store<usize, usize> = Store::new();
     let s2: Stash<usize, usize> = Stash::new();
     let s3: UniqueStash<usize> = UniqueStash::new();
     let s4: SlotMap<DefaultKey, usize> = SlotMap::new();
 
-    let mut g = c.benchmark_group("Fresh Inserts");
+    let mut g = c.benchmark_group("Inserts");
     g.bench_function("Store", |b| {
         b.iter_batched_ref(
             || s1.clone(),
@@ -58,6 +58,72 @@ fn fresh_inserts(c: &mut Criterion) {
     });
 }
 
+fn reinserts(c: &mut Criterion) {
+    let size = 10_000;
+    let mut s1: Store<usize, usize> = Store::new();
+    let mut s2: Stash<usize, usize> = Stash::new();
+    let mut s3: UniqueStash<usize> = UniqueStash::new();
+    let mut s3k = Vec::new();
+    let mut s4: SlotMap<DefaultKey, usize> = SlotMap::new();
+    let mut s4k = Vec::new();
+    for a in 0..size {
+        s1.insert(a);
+        s2.put(a);
+        s3k.push(s3.put(a));
+        s4k.push(s4.insert(a));
+    }
+    for a in 0..size {
+        s1.remove(a);
+        s2.take(a);
+        s3.take(s3k[a]);
+        s4.remove(s4k[a]);
+    }
+    let mut g = c.benchmark_group("Re-inserts");
+    g.bench_function("Store", |b| {
+        b.iter_batched_ref(
+            || s1.clone(),
+            |i| {
+                for a in 0..size {
+                    i.insert(a);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+    g.bench_function("Stash", |b| {
+        b.iter_batched_ref(
+            || s2.clone(),
+            |i| {
+                for a in 0..size {
+                    i.put(a);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+    g.bench_function("UniqueStash", |b| {
+        b.iter_batched_ref(
+            || s3.clone(),
+            |i| {
+                for a in 0..size {
+                    i.put(a);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+    g.bench_function("SlotMap", |b| {
+        b.iter_batched_ref(
+            || s4.clone(),
+            |i| {
+                for a in 0..size {
+                    i.insert(a);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+}
 fn remove(c: &mut Criterion) {
     let size = 10_000;
     let mut rng = thread_rng();
@@ -301,5 +367,5 @@ fn iter(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, fresh_inserts, remove, get, iter);
+criterion_group!(benches, inserts, reinserts, remove, get, iter);
 criterion_main!(benches);
